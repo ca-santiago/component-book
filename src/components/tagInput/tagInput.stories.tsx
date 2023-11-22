69a4233ecc3e15ea3a43e560994f947d574494bc
Carmen <ca.santiago.me@gmail.com>
2023-11-21 20:37:33 -0600

    feat: udpate tag input documentation

--- src/components/tagInput/tagInput.stories.tsx
+++ src/components/tagInput/tagInput.stories.tsx
@@ -3,10 +3,40 @@ import React from "react";
 import TagInput from "./tagInput";
 
 export default {
-  title: "TagInput",
+  title: "Tag input",
   component: TagInput,
 };
 
 export const Normal = () => {
-  return <TagInput />;
+  const [tags, setTags] = React.useState<string[]>([]);
+  return (
+    <div>
+      <div>
+        {tags.map((t) => (
+          <p>{t}</p>
+        ))}
+      </div>
+      <br />
+      <h2>Add your tags here</h2>
+      <TagInput onTagsChange={setTags} />
+    </div>
+  );
+};
+
+export const InitialTags = () => {
+  const somePrefilledTags = ["some", "prefilled", "tags"];
+  // onTagsChange will not notify back when the initialTags is set, but when a new tag is added
+  const [tags, setTags] = React.useState<string[]>([]);
+  return (
+    <div>
+      <div>
+        {tags.map((t) => (
+          <p>{t}</p>
+        ))}
+      </div>
+      <br />
+      <h2>Add your tags here</h2>
+      <TagInput initialTags={somePrefilledTags} onTagsChange={setTags} />
+    </div>
+  );
 };
